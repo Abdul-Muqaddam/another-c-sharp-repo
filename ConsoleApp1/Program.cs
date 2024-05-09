2af76714490bde204a62999345c32f9382fa8c0e
Abdul-Muqaddam <chandansar444999@gmail.com>
2024-05-10 04:12:04 +0500

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello this is yousaf how can i help you ");
+            Console.ReadLine();
+        }
+    }
+}
